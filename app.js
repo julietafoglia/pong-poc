9bf442042d3de221b96cf0f5917fb783dddeb066
julietafoglia <julieta.foglia@valtech.com>
2022-08-10 10:49:23 -0300

    Add websocket

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const WebSocket = require("ws");
 
 const app = express();
 
@@ -13,7 +14,14 @@ app.get("/", (req, res) => {
   });
 });
 
-app.listen(9000, "127.0.0.1", () => {
+const server = app.listen(9000, "127.0.0.1", () => {
   console.log("\x1b[36m", "Server is running on port 9000");
   console.log("\x1b[1m", "-> Go to http://localhost:9000");
+});
+
+//initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (ws) => {
+  ws.send("Hi there");
 });
